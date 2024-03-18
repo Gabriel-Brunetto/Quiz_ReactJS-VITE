680eedae585dfd1d61716328c81bbda7a7bba18e
Gabriel Brunetto <gabibq@yahoo.com.br>
2024-03-18 15:11:18 -0300

    Upando arquivos para repostorio, para versioanamento e alterações

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import Welcome from './components/Welcome.jsx'
+import './App.css'
+
+function App() {
+  
+  return (
+    
+      <div className="App">
+        <h1>Quiz De Programação</h1>
+        <Welcome />:
+      </div>
+        
+  )
+}
+
+export default App
