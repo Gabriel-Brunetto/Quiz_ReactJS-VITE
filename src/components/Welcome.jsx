88d5ed24800501e9df4018f4ef48ea133fb33b85
Gabriel Brunetto <gabibq@yahoo.com.br>
2024-03-19 08:57:09 -0300

    Estilizando pagina e adicionado QuizContext e QuizProvider e simulado retorno em JS com console.log(quizState)

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,7 +1,14 @@
+import { useContext } from "react";
+import { QuizContext } from "../context/quiz";
+
 import Quiz from "../img/quiz.svg";
 import "./Welcome.css";
 
 const Welcome = () => {
+  const quizState = useContext(QuizContext);
+
+  console.log(quizState);
+  
   return (
     <div id="welcome">
         <h2>Seja Bem Vindo</h2>
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { QuizProvider } from './context/quiz.jsx'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <QuizProvider>
+      <App>
+        
+      </App>
+    </QuizProvider>
   </React.StrictMode>,
 )
